63fd3ddc85ee994b5d546bc2d4d03fa09384b95b
Dmitry <dmitry.korchagin.s@gmail.com>
2024-06-14 14:58:04 +0300

    Initial Commit

---
+++ DailyDeeds/DailyDeedsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyDeedsApp.swift
+//  DailyDeeds
+//
+//  Created by Дмитрий Корчагин on 6/14/24.
+//
+
+import SwiftUI
+
+@main
+struct DailyDeedsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
