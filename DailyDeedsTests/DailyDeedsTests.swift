bc154a63fdb144e6e5ba295486ddbbe8771c7162
Dmitry <123954892+tzopiz@users.noreply.github.com>
2024-06-21 18:18:08 +0300

    Revert "Реализация структуры TodoItem, расширения для работы с JSON и CSV объ…"
    This reverts commit b4dba07283c8e58bde3fa3349095aa3446e3ce53.

---
+++ DailyDeedsTests/DailyDeedsTests.swift
@@ -0,0 +1,17 @@
+//
+//  DailyDeedsTests.swift
+//  DailyDeedsTests
+//
+//  Created by Дмитрий Корчагин on 6/14/24.
+//
+
+import Testing
+@testable import DailyDeeds
+
+struct DailyDeedsTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
